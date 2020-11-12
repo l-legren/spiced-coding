8a90c86202c8d669ab710b6d7117b456f48e1a44
Carlos Leret <carlosleret@gmail.com>
2020-11-12 16:40:00 +0100

    fetching with ajax completed, ticker still not working

--- week_3/ticker/script.js
+++ week_3/ticker/script.js
@@ -1,63 +1,55 @@
 (function () {
-    ticker("ticker1", -1);
 
-    ticker("ticker2", 1);
-
-    function ticker(id, step) {
-        // var ticker = document.getElementById(id);
-        var ticker = $(id);
-        // var headlines = ticker.querySelector(".headlines");
+    
+    function ticker() {
+        var ticker = $("#ticker");
         var headlines = ticker.children(".headlines");
-        // var links = headlines.getElementsByTagName("A");
         var links = headlines.children("a");
-        // var curX = headlines.offsetLeft;
-        var curx = headlines.offset().left;
-        // var headlinesWidth = headlines.offsetWidth;
+        var curX = headlines.offset().left;
         var headlinesWidth = headlines.width();
-        // var tickerWidth = ticker.offsetWidth;
         var tickerWidth = ticker.width();
-        var linkWidth =
-            step < 0
-                ?  links.eq(0).width() // links[0].offsetWidth //
-                :  links.eq(links.length - 1).width(); // links[links.length - 1].offsetWidth; //;
+        var linkWidth = links.eq(0).width();
         var animId;
+        
+        $.ajax({
+            type: "GET",
+            url: "/data.json",
+            success: function (response) {
+                for (var i = 0; i < response.length; i++) {
+                    links.eq(i).html(response[i].text);
+                    links.attr("href", response[i].link);
+                }     
+            }
+        });
+        
+        console.log(curX);
+        console.log(linkWidth);
 
-        // headlines.addEventListener("mouseenter", function (e) {
-        //     cancelAnimationFrame(animId);
-        // });
-
-        headlines.on("mouseenter", function (e){
+        headlines.on("mouseenter", function (){
             cancelAnimationFrame(animId);
         });
 
-        // headlines.addEventListener("mouseleave", function () {
-        //     moveHeadlines();
-        // });
-
-        headlines.on("mouseleave", function(e){
+        headlines.on("mouseleave", function(){
             moveHeadlines();
         });
 
         moveHeadlines();
 
         function moveHeadlines() {
-            curX += step;
-            if (step < 0 && curX < -linkWidth) {
+            curX --;
+            if (curX < -linkWidth) {
                 curX += linkWidth;
                 // headlines.appendChild(links[0]);
                 headlines.append(links.eq(0));
                 // linkWidth = links[0].offsetWidth;
                 linkWidth = links.eq(0).width();
             }
-            if (step > 0 && curX + headlinesWidth > tickerWidth + linkWidth) {
-                curX -= linkWidth;
-                // headlines.insertBefore(links[links.length - 1], links[0]);
-                headlines.before(links.eq(links.length - 1), links.eq(0));
-                // linkWidth = links[links.length - 1].offsetWidth;
-                linkWidth = links.eq(links.length - 1).width();
-            }
-            headlines.style.left = curX + "px";
+
+            headlines.offset().left = curX + "px";
             animId = requestAnimationFrame(moveHeadlines);
         }
     }
+
+    ticker();
+
 })();
