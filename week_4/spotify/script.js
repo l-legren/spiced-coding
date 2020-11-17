e1c9728817b65d31e09d9efbdefc42c84f4961f9
Carlos Leret <carlosleret@gmail.com>
2020-11-17 15:44:26 +0100

    completed spotify search with infinite scroll

--- week_4/spotify/script.js
+++ week_4/spotify/script.js
@@ -1,8 +1,9 @@
 (function () {
+    var nextUrl;
+
     $("#submit-button").on("click", function (e) {
         var userInput = encodeURIComponent($("#input").val());
         var albumOrArtist = $("select").val();
-        var nextUrl;
         
         $.ajax({
             method: "GET",
@@ -11,52 +12,30 @@
                 query: userInput,
                 type: albumOrArtist
             },
-            success: function showResults(response) {
+            success: function (response) {
                 response = response.artists || response.albums;
-                
+
+                if (location.search.indexOf("scroll=infinite") > -1) {
+                    checkScrollPosition();
+                }
+
                 if (response.items[0]) {
                     $("#results-found").html('Results for "' + userInput + '"');
                     $("#results-container").addClass("frame-results");
-                    // $("#more").css("visibility", "visible");
                 } else {
                     $("#results-found").html("No results found");
                 }
-
-                var myHtml = "";
                 
-                for (var i = 0; i < response.items.length; i++) {
-                    
-                    var imageUrl = "/default.jpg";
-                    if (response.items[i].images[0]) {
-                        imageUrl = response.items[i].images[0].url;
-                    }
-                    
-                    myHtml += "<div class='single-result-container'><img class='result-image' src=" + imageUrl + " alt='album-image'/>" + "<div class='result'>" + response.items[i].name + "</div></div>";
-                }
-                
-                console.log(response);
-                $("#results-container").html(myHtml);
+                $("#results-container").html(showResults(response));
 
                 $("#results-container").append(
                     '<div id="button-container"><button class="button" id="more">More</button></div>'
                 );
 
-
-                // When I press the more button should make another request
-
-                nextUrl =
-                    response.next &&
-                    response.next.replace(
-                        "https://api.spotify.com/v1/search",
-                        "https://spicedify.herokuapp.com/spotify"
-                    );
+                setNextUrl(response.next);
                 
-                console.log($("#more"));
-
                 $("#more").on("click", function () {
-                    // console.log(nextUrl);
                     $.ajax({
-                        method: "GET",
                         url: nextUrl,
                         success: showResults,
                     });
@@ -64,4 +43,59 @@
             }
         });
     });
+
+    function checkScrollPosition() {
+
+        setTimeout(function() {
+            if (
+                $(window).height() + $(document).scrollTop() 
+                >= $(document).height() - 200 
+            ) {
+                console.log("Bottom was reached!");
+                $("#button-container").remove();
+                $.ajax({
+                    // method: "GET",
+                    url: nextUrl,
+                    success: function (newResponse) {
+                        newResponse = newResponse.artists || newResponse.albums;
+                        $("#results-container").append(showResults(newResponse));
+                        checkScrollPosition();
+                    },
+                });
+            } else {
+                checkScrollPosition();
+            }    
+        }, 500);
+    }
+
+    function setNextUrl(next) {
+        if (next) {
+            nextUrl = next.replace(
+                "https://api.spotify.com/v1/search",
+                "https://spicedify.herokuapp.com/spotify"
+            );
+        }
+    }
+
+    function showResults(payload) {
+        
+        var myHtml = "";
+
+        for (var i = 0; i < payload.items.length; i++) {
+            var imageUrl = "/default.jpg";
+            if (payload.items[i].images[0]) {
+                imageUrl = payload.items[i].images[0].url;
+            }
+
+            myHtml +=
+                "<div class='single-result-container'><img class='result-image' src=" +
+                imageUrl +
+                " alt='album-image'/>" +
+                "<div class='result'>" +
+                payload.items[i].name +
+                "</div></div>";
+        }
+        return myHtml;
+    }
+
 })();
