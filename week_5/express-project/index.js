2ae59d73b368e32e67e96e7269d91fdf3f8017b1
Carlos Leret <carlosleret@gmail.com>
2020-11-26 12:04:13 +0100

    express project, auth not working

--- week_5/express-project/index.js
+++ week_5/express-project/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
-
 const app = express();
+const cookieParser = require("cookie-parser");
+const { projectList } = require("./projectList");
+const basicAuth = require("basic-auth");
+
+const auth = function (req, res, next) {
+    const creds = basicAuth(req);
+    if (!creds || creds.name != "discoduck" || creds.pass != "opensesame") {
+        res.setHeader(
+            "WWW-Authenticate",
+            'Basic realm="Enter your credentials to see this stuff."'
+        );
+        res.sendStatus(401);
+    } else {
+        next();
+    }
+};
 
 app.use(
     express.urlencoded({
@@ -14,10 +28,24 @@ app.use(cookieParser());
 app.use(express.static("./projects"));
 
 app.get("/", (req, res) => {
-    console.log(req.cookies.cookies);
+    // console.log(req.cookies.cookies);
     res.cookie("cookies", false);
     if (req.cookies.cookies === "true") {
-        res.sendFile(`${__dirname}/index.html`);
+        res.send(
+            `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <h1>Having some cookies fun</h1>
+                <h2>It's gonna be cooklicious 😁</h2>
+                $projectList()
+            </body>
+            </html>`
+        );
     } else {
         res.redirect("/cookies");
     }
@@ -28,8 +56,8 @@ app.get("/cookies", (req, res) => {
 });
 
 app.post("/cookies", (req, res) => {
-    console.log("POST request was made to /cookies");
-    console.log(req.body);
+    // console.log("POST request was made to /cookies");
+    // console.log(req.body);
     const { cookies } = req.body;
     if (cookies) {
         res.cookie("cookies", true);
@@ -40,6 +68,12 @@ app.post("/cookies", (req, res) => {
             "<h1>No cookies no site</h1><p>🛑 Without cookies you can not navigate the site 🛑</p>"
         );
     }
+});
+
+
+app.get("/connect4", (req, res) => {
+    app.use(auth);
+    res.sendFile(`${__dirname}/projects/connect4/index.html`);
 });
 
 app.listen(8080, () => console.log("Listening to server on port 8080"));
---
+++ week_5/express-project/projectList.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const projectList = () => {
+    // get a list of all your projects that are inside your projects folder (you'll get an array of names) - use readdirSync
+    const myDir = `${__dirname}/projects`;
+    const filesSync = fs.readdirSync(myDir, { withFileTypes: true });
+    // loop through your list and create a string of html and a link for each item in the directory
+    var htmlString = "";
+    for (let i = 0; i < filesSync.length; i++) {
+        htmlString += `<a href="/${filesSync[i].name}">${filesSync[i].name}</a><br>`;
+    }
+    // make sure you RETURN the completed html string
+    return htmlString;
+};
+
+module.exports.projectList = projectList;
+
+console.log(projectList());
